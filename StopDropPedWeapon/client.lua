3915f11308e10be3a81c8ae16bdf35e434886290
Dracke <36110355+Dracke39@users.noreply.github.com>
2020-01-25 23:48:00 +0100

    Add files via upload

---
+++ StopDropPedWeapon/client.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(1)
+    RemoveAllPickupsOfType(0xDF711959)
+    RemoveAllPickupsOfType(0xF9AFB48F)
+    RemoveAllPickupsOfType(0xA9355DCD)
+  end
+end)
