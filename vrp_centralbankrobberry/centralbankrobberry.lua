69760d76ff147e11605af454ebee1f98b1c5faee
Dracke <36110355+Dracke39@users.noreply.github.com>
2020-02-06 22:06:30 +0100

    Add files via upload

---
+++ vrp_centralbankrobberry/centralbankrobberry.lua
@@ -0,0 +1,8 @@
+
+local items = {}
+
+items["bank_docs"] = {"Bank documents","",nil,0.0}
+items["bank_drill"] = {"Drill","",nil,1.0}
+items["bank_phone"] = {"Hacking Phone","",nil,0.2}
+
+return items
