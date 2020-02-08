15d7e494caef7322baa39eb579289e4290f72f3c
Dracke <36110355+Dracke39@users.noreply.github.com>
2020-02-08 19:55:26 +0100

    Update client.lua

--- MedicHealt/client.lua
+++ MedicHealt/client.lua
@@ -1,8 +1,9 @@
 positions = {
-    -- Blip cords, tp bed, tp respawn, presskey
-    -- X Y Z Heading
-    {{307.92263793945,-595.06616210938,42.341786193848},{354.1682434082,-592.81365966797,43.104415893555, 336.63},{353.46752929688,-592.33630371094,43.315010070801, 19.78}, 38},
+    {307.92263793945,-595.06616210938,42.341786193848, 38},
 }
+
+PlayerProps = {}
+PlayerHasProp = false
 
 Citizen.CreateThread(function ()
     while true do
@@ -12,59 +13,54 @@ Citizen.CreateThread(function ()
 
         for _,location in ipairs(positions) do
 
-            pos1 = {
-                x=location[1][1],
-                y=location[1][2],
-                z=location[1][3],
-            }
-            pos2 = {
-                x=location[2][1],
-                y=location[2][2],
-                z=location[2][3],
-                heading=location[2][4]
-            }
-            pos3 = {
-                x=location[3][1],
-                y=location[3][2],
-                z=location[3][3],
-                heading=location[3][4]
-            }
-
+            x = location[1]
+            y = location[2]
+            z = location[3]
             presskey = location[4]
 
             DrawMarker(27, x, y, z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, 0, 127, 255, 200, 0, 0, 0, 1)
 
-            if position_verf(playerLoc.x, playerLoc.y, playerLoc.z, pos1.x, pos1.y, pos1.z, 2) then
-                help_message("Press ~INPUT_CONTEXT~ to sign the form")
+            if position_verf(playerLoc.x, playerLoc.y, playerLoc.z, x, y, z, 2) then
+                help_message("Press ~INPUT_CONTEXT~ to sign the medical form")
                 if IsControlJustReleased(1, presskey) then
                     FreezeEntityPosition(GetPlayerPed(-1), true)
-                    notify_message("~g~You are signing the release")
+                    notify_message("~g~Stai firmando la liberatoria")
                     loadAnimDict( "amb@medic@standing@timeofdeath@enter" )
                     loadAnimDict( "amb@medic@standing@timeofdeath@base" )
                     loadAnimDict( "amb@medic@standing@timeofdeath@exit" )
                     TaskPlayAnim(GetPlayerPed(-1),"amb@medic@standing@timeofdeath@enter", "enter",1.0, -1.0, 7000, 0, 7, false, false, false)
                     TaskPlayAnim(GetPlayerPed(-1),"amb@medic@standing@timeofdeath@base", "base",1.0, -1.0, 7000, 0, 7, false, false, false)
                     TaskPlayAnim(GetPlayerPed(-1),"amb@medic@standing@timeofdeath@exit", "exit",1.0, -1.0, 7000, 0, 7, false, false, false)
-                    Citizen.Wait(7000)
-                    DoScreenFadeOut(500)
-                    Citizen.Wait(500)
-                    SetEntityCoords(GetPlayerPed(-1), pos2.x, pos2.y, pos1.z, 1,0,0,1)
-                    SetEntityHeading(GetPlayerPed(-1), pos2.heading)
-                    TaskPlayAnim(GetPlayerPed(-1),"anim@gangops@morgue@table@", "body_search",1.0, -1.0, 120000, 1, 120, false, false, false)
-                    Citizen.Wait(1500)
-                    DoScreenFadeIn(500)
-                    notify_message("~g~The medical staff is taking care of you")
-                    DrawSpecialText("The medical staff is taking care of you", 120000)
-                    Citizen.Wait(120000)
-                    notify_message("~g~The medical staff has provided all the medical care you need now you are free")
-                    FreezeEntityPosition(GetPlayerPed(-1), false)
-                    setHealth(200)
-                    DoScreenFadeOut(500)
-                    Citizen.Wait(500)
-                    SetEntityCoords(GetPlayerPed(-1), pos3.x, pos3.y, pos3.z, 1,0,0,1)
-                    SetEntityHeading(GetPlayerPed(-1), pos3.heading)
-                    Citizen.Wait(1000)
-                    DoScreenFadeIn(1000)
+                    AddPropToPlayer2("prop_pencil_01", 58866, 0.11, -0.02, 0.001, -100.0, 0.0, 0.0)
+                    AddPropToPlayer("prop_notepad_01", 18905, 0.1, 0.02, 0.05, 10.0, 0.0, 0.0)
+                    SetTimeout(7000, function()
+                        DoScreenFadeOut(500)
+                        SetTimeout(500, function()
+                            DestroyAllProps()
+                            SetEntityCoords(GetPlayerPed(-1), 354.1682434082,-592.81365966797,43.104415893555, 1,0,0,1)
+                            SetEntityHeading(GetPlayerPed(-1), 336.63)
+                            TaskPlayAnim(GetPlayerPed(-1),"anim@gangops@morgue@table@", "body_search",1.0, -1.0, 120000, 1, 120, false, false, false)
+                            SetTimeout(1500, function()
+                                DoScreenFadeIn(500, function()
+                                notify_message("~g~The medical staff is taking care of you")
+                                DrawSpecialText("The medical staff is taking care of you", 120000)
+                                SetTimeout(120000, function()
+                                    notify_message("~g~The medical staff has provided all the medical care you need now you are free")
+                                    FreezeEntityPosition(GetPlayerPed(-1), false)
+                                    setHealth(200)
+                                    DoScreenFadeOut(500)
+                                    SetTimeout(500, function()
+                                        SetEntityCoords(GetPlayerPed(-1), 353.46752929688,-592.33630371094,43.315010070801, 1,0,0,1)
+                                        SetEntityHeading(GetPlayerPed(-1), 19.78)
+                                        SetTimeout(1000, function()
+                                            DoScreenFadeIn(1000)
+                                            end)
+                                        end)
+                                    end)
+                                end)
+                            end)
+                        end)
+                    end)
                 end
             end
         end
@@ -97,6 +93,13 @@ function notify_message(msg)
     DrawNotification(true, false)
 end
 
+function notifyPicture(icon, type, sender, title, text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    SetNotificationMessage(icon, icon, true, type, sender, title, text)
+    DrawNotification(false, true)
+end
+
 function setHealth(health)
     local n = math.floor(health)
     SetEntityHealth(GetPlayerPed(-1),n)
@@ -114,3 +117,54 @@ function DrawSpecialText(m_text, showtime)
 	AddTextComponentString(m_text)
 	DrawSubtitleTimed(showtime, 1)
 end
+
+function LoadPropDict(model)
+    while not HasModelLoaded(GetHashKey(model)) do
+      RequestModel(GetHashKey(model))
+      Wait(10)
+    end
+end
+  
+function DestroyAllProps()
+    for _,v in pairs(PlayerProps) do
+      DeleteEntity(v)
+    end
+    PlayerHasProp = false
+end
+  
+function AddPropToPlayer(prop1, bone, off1, off2, off3, rot1, rot2, rot3)
+    local Player = PlayerPedId()
+    local x,y,z = table.unpack(GetEntityCoords(Player))
+  
+    if not HasModelLoaded(prop1) then
+      LoadPropDict(prop1)
+    end
+  
+    prop = CreateObject(GetHashKey(prop1), x, y, z+0.2,  true,  true, true)
+    AttachEntityToEntity(prop, Player, GetPedBoneIndex(Player, bone), off1, off2, off3, rot1, rot2, rot3, true, true, false, true, 1, true)
+    table.insert(PlayerProps, prop)
+    PlayerHasProp = true
+    SetModelAsNoLongerNeeded(prop1)
+end
+
+function AddPropToPlayer2(prop2, bone, off1, off2, off3, rot1, rot2, rot3)
+    local Player = PlayerPedId()
+    local x,y,z = table.unpack(GetEntityCoords(Player))
+  
+    if not HasModelLoaded(prop2) then
+      LoadPropDict(prop2)
+    end
+  
+    prop21 = CreateObject(GetHashKey(prop2), x, y, z+0.2,  true,  true, true)
+    AttachEntityToEntity(prop21, Player, GetPedBoneIndex(Player, bone), off1, off2, off3, rot1, rot2, rot3, true, true, false, true, 1, true)
+    table.insert(PlayerProps, prop21)
+    PlayerHasProp = true
+    SetModelAsNoLongerNeeded(prop2)
+end
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+      DestroyAllProps()
+      DoScreenFadeIn(5)
+    end
+end)
