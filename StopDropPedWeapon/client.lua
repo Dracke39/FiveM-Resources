88d31f9d9dedc788c10c3cf8bfc90476d54a6d81
Dracke <36110355+Dracke39@users.noreply.github.com>
2020-02-09 20:40:56 +0100

    Update client.lua

--- StopDropPedWeapon/client.lua
+++ StopDropPedWeapon/client.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
   while true do
-    Citizen.Wait(1)
+    Citizen.Wait(500)
     RemoveAllPickupsOfType(0xDF711959)
     RemoveAllPickupsOfType(0xF9AFB48F)
     RemoveAllPickupsOfType(0xA9355DCD)
