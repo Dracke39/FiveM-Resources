cdb78b91f29239b520d56221ff6d0e17f94e404f
Dracke <36110355+Dracke39@users.noreply.github.com>
2020-03-03 12:46:43 +0100

    Add files via upload

---
+++ InteractNPCLocation/client.lua
@@ -0,0 +1,91 @@
+markets = {
+    -- Markets
+    {372.98,328.13,103.57,245.0,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {2676.57,3280.19,55.24,328.08,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1959.1,3741.48,32.34,296.51,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1392.33,3606.27,34.98,197.18,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1697.1,4923.52,42.06,327.59,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1728.39,6416.75,35.04,238.52,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1165.41,2710.95,38.15,178.92,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {549.33,2669.77,42.15,111.27,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-3243.93,1000.02,12.83,352.04,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-3040.49,583.88,7.91,17.94,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-2966.18,391.49,15.04,82.43,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1819.45,793.68,138.08,132.59,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1486.59,-377.41,40.16,125.62,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1221.37,-907.95,12.33,30.08,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-706.04,-914.4,19.22,90.99,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {24.38,-1345.72,29.5,271.7,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-47.24,-1758.68,29.42,54.73,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1134.22,-983.14,46.42,279.1,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1164,-323.63,691.21,92.28,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {2555,380.83,108.62,4.25,"mp_m_shopkeep_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    -- Ammunations
+    {810.39,-2159.13,29.62,7.62,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {23.15,-1105.72,29.8,153.24,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {842.19,-1035.34,28.19,356.79,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-661.81,-933.35,21.83,192.2,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1304.06,-394.9,36.7,77.25,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {2567.4,292.6,108.73,358.85,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-3173.57,1088.71,20.84,248.39,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1118.99,2699.88,18.55,224.92,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-331.4,6085.45,31.45,218.1,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1692.51,3761.17,34.7,219.1,"s_m_y_ammucity_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    -- Dess shop
+    {73.93,-1392.26,29.38,267.43,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {427.02,-806.97,29.49,75.06,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-822.66,-1071.97,11.33,204.33,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1194.81,-767.6,17.32,214.21,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-164.93,-302.71,39.73,238.74,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {127.41,-223.13,54.56,72.94,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-708.72,-152.07,37.42,113.2,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1448.94,-238.19,49.81,47.67,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-3168.91,1044.25,20.86,62.7,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {-1102.26,2711.85,19.11,221.31,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {612.98,2761.76,42.09,268.95,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1197.04,2711.8,38.22,175.41,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {1695.5,4822.63,42.06,82.89,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    {5.59,6511.08,31.88,39.51,"u_f_y_hotposh_01","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    -- Sheriff
+    {-448.49,6012.44,31.72,314.12,"csb_cop","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    -- Police
+    {440.67,-978.87,30.69,180.03,"csb_prolsec","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+    -- Simeon
+    {-32.13,-1114.37,26.42,70.06,"cs_siemonyetarian","amb@world_human_hang_out_street@female_arms_crossed@base","base"},
+
+}
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    for _,location in ipairs(markets) do
+        x = location[1]
+        y = location[2]
+        z = location[3]
+        heading = location[4]
+
+        model = location[5]
+
+        anim = location[6]
+        animbone = location[7]
+
+        local Hash = GetHashKey(model)
+        local Ped = CreatePed(1, Hash, x,y,z-0.95, 0.0, false, true)
+        if not HasModelLoaded(Hash) then
+            RequestModel(Hash)
+            Citizen.Wait(200)
+        end
+        SetEntityHeading(Ped, heading)
+        FreezeEntityPosition(Ped, true)
+        loadAnimDict( anim )
+        TaskPlayAnim(Ped, anim, animbone, 1.0,-1.0, -1, 1, 1, true, true, true)
+    end
+end)
+
+function loadAnimDict( dict )
+    while ( not HasAnimDictLoaded( dict ) ) do
+        RequestAnimDict( dict )
+        Citizen.Wait(5)
+    end
+end
